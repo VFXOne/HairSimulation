cde526dc3bbb87dd5c71cd102c1c20dbc3bf8947
Lucas Strauss <lucas.strauss@epfl.ch>
2019-09-19 11:23:13 +0200

    trying to cast the mesh into edges but unsuccessful

---
+++ applet/build/nanogui/ext_build/glfw/src/CMakeFiles/glfw_objects.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/glfw_objects.dir/context.c.o"
+  "CMakeFiles/glfw_objects.dir/init.c.o"
+  "CMakeFiles/glfw_objects.dir/input.c.o"
+  "CMakeFiles/glfw_objects.dir/monitor.c.o"
+  "CMakeFiles/glfw_objects.dir/vulkan.c.o"
+  "CMakeFiles/glfw_objects.dir/window.c.o"
+  "CMakeFiles/glfw_objects.dir/x11_init.c.o"
+  "CMakeFiles/glfw_objects.dir/x11_monitor.c.o"
+  "CMakeFiles/glfw_objects.dir/x11_window.c.o"
+  "CMakeFiles/glfw_objects.dir/xkb_unicode.c.o"
+  "CMakeFiles/glfw_objects.dir/linux_joystick.c.o"
+  "CMakeFiles/glfw_objects.dir/posix_time.c.o"
+  "CMakeFiles/glfw_objects.dir/posix_tls.c.o"
+  "CMakeFiles/glfw_objects.dir/glx_context.c.o"
+  "CMakeFiles/glfw_objects.dir/egl_context.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/glfw_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
